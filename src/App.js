d9ebd180f8f5b9146a4de78d8804771154882ed5
Richard <rkj128@gmail.com>
2020-08-12 10:36:05 -0700

    bug fixes on components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './components/Todos';
 
 import './App.css';
 
-function App() {
+class App extends Component {
 
   state = {
     todos: [
       {
         id: 1,
-        title: 'Take out the Trash',
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Take out the trash',
         completed: false
       }
     ]
   }
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
+
 }
 
 export default App;
