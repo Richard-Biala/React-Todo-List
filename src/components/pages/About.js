05b367e490e6be5ce403abddc066b18256668afe
Richard <rkj128@gmail.com>
2020-08-12 16:40:59 -0700

    final updates completed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function About() {
+function About() {
     return (
-        <div>
-
-        </div>
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app v1.0.0</p>
+        </React.Fragment>
     )
 }
+
+
+export default About;
