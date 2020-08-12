7d989d47edeb3579c09a2aa163cf5a98b0926aa0
Richard <rkj128@gmail.com>
2020-08-12 15:24:14 -0700

    layout updates

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -22,6 +22,7 @@ export class TodoItem extends Component {
                 <p>
                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
                     {title}
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
             </div >
         )
@@ -29,8 +30,19 @@ export class TodoItem extends Component {
 }
 
 
-const itemStyle = {
-    backgroundColor: '#f4f4f4'
+//PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem
